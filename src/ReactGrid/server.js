01f9a3d645c25b61c269dd74ae820812ee9fd78a
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2016-08-08 12:10:20 +0300

    edited proxy

--- src/ReactGrid/server.js
+++ src/ReactGrid/server.js
@@ -8,7 +8,7 @@ new WebpackDevServer(webpack(config), {
   historyApiFallback: true,
   proxy: {
       '*': {
-          target: 'http://localhost:59600',
+          target: 'http://localhost:5000',
           secure: false
       }
   }
