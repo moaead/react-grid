8914d8fa4ee4771e5e6580fdbabb3cc2e0e6cb5d
Moaead Yahya <moaead@foothillsolutions.com>
2016-08-08 10:37:45 +0300

    add datatables, and datatables-semantic

--- src/ReactGrid/src/index.js
+++ src/ReactGrid/src/index.js
@@ -5,6 +5,11 @@ var react_router_1 = require("react-router");
 var routes_1 = require("./routes");
 require("../wwwroot/libs/semantic-ui/dist/semantic.min.js");
 require("../wwwroot/libs/semantic-ui/dist/semantic.min.css");
+require("datatables.net");
+require("datatables.net-dt/css/jquery.dataTables.css");
+require("../wwwroot/libs/datatables-semantic-ui/dataTables.semanticui.min.css");
+require("../wwwroot/libs/datatables-semantic-ui/dataTables.semanticui.min.js");
+require("../wwwroot/libs/datatables-semantic-ui/buttons.semanticui.min");
 $(document).ready(function () {
     ReactDOM.render(React.createElement(react_router_1.Router, {history: react_router_1.hashHistory}, routes_1.default.getRoutes()), document.getElementById('app'));
 });
--- src/ReactGrid/src/index.tsx
+++ src/ReactGrid/src/index.tsx
@@ -5,6 +5,10 @@ import Routes from "./routes";
 
 require("../wwwroot/libs/semantic-ui/dist/semantic.min.js");
 require("../wwwroot/libs/semantic-ui/dist/semantic.min.css");
+require("datatables.net");
+require("datatables.net-dt/css/jquery.dataTables.css");
+require("../wwwroot/libs/datatables-semantic-ui/dataTables.semanticui.min.css");
+require("../wwwroot/libs/datatables-semantic-ui/dataTables.semanticui.min.js");
 
 $(document).ready(() => {
     ReactDOM.render(<Router history={hashHistory}>{Routes.getRoutes()}</Router>, document.getElementById('app'));
